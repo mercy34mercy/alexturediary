c66453930aa540a3cbdd2b19e99fa2234b26f5b1
mercy34mercy <is0558fs@ed.ritsumei.ac.jp>
2022-09-17 00:21:48 +0900

    明日からはインターン

--- src/Fetch.tsx
+++ src/Fetch.tsx
@@ -1,31 +1,31 @@
 import React from 'react';
 import { useGetuserQuery } from './generated/graphql'
- 
+
 
 export const MyComponent = () => {
-    const {  data, error, loading } = useGetuserQuery({  variables: {argument: "abcdef"},});
-    if(loading) {
+    const { data, error, loading } = useGetuserQuery({ variables: { argument: "abcdef" }, });
+    if (loading) {
         return <p>...loading</p>
-      }
-      
-      if(error) {
+    }
+
+    if (error) {
         return <p>Error!</p>
-      }
+    }
 
-      if(data == undefined){
+    if (data == undefined) {
         return <p>Error!</p>
-      }
+    }
 
-
-      return (
+    return (
         <ul>
-          {data.User.Diary.map((d) =>(
-            <li key={d.Diaryid}>
-                <p>{d.Word}</p>
-                <img src={d.Imageurl} alt="" />
-                <p>{d.CreatedAt}</p>
-            </li>
-          ))}
+            {data.User.Diary.map((d) => (
+                <li key={d.Diaryid}>
+                    <p>{d.User.Name}</p>
+                    <p>{d.Word}</p>
+                    <img src={d.Imageurl} alt="" />
+                    <p>{d.CreatedAt}</p>
+                </li>
+            ))}
         </ul>
-      );
+    );
 }
