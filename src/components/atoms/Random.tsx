f6132a49f8ea020717bc9f30a35d9faa4325833c
Syu <mirzamrion@gmail.com>
2022-09-22 11:30:14 +0900

    ready for commit

--- src/components/atoms/Random.tsx
+++ src/components/atoms/Random.tsx
@@ -1,72 +1,71 @@
 import { Any } from "@react-spring/types";
 import React from "react";
-import '../../stylesheets/animation.css'
+import "../../stylesheets/animation.css";
+
 
 type prop = {
-  emotion: object
-}
+  emotion: object;
+};
 
 export const RandomObject = () => {
-  const randomWidth = Math.floor(Math.random() *window.outerWidth);
+  const randomWidth = Math.floor(Math.random() * window.outerWidth);
   const randomAnimationDulation = Math.random() * 2 + 4;
   const randomHeight = Math.floor(Math.random() * 300);
-  const randomsSize = Math.floor( Math.random() * 100);
+  const randomsSize = Math.floor(Math.random() * 100);
 
-  // console.log(prop.emotion);
-
-  const inputemotion = "Sad"
-  let displayemotion = ""
+  const inputemotion = "Sad";
+  let displaycolor = "";
   var arr = [
     {
       id: 1,
       emotion: "Fear",
-      color: "skyblue"
+      color: "skyblue",
     },
     {
       id: 2,
       emotion: "Sad",
-      color: "blue"
+      color: "blue",
     },
     {
       id: 3,
       emotion: "Surprise",
-      color: "yellow"
+      color: "yellow",
     },
     {
       id: 4,
       emotion: "Happy",
-      color: "orange"
+      color: "orange",
     },
-  ]
-  
-  for(var i=0;i<5;i++){
-    if(arr[i].emotion == inputemotion)
-    displayemotion = arr[i].color
-  }
-  
+  ];
+
+  if (arr[i].emotion == inputemotion) displaycolor = arr[i].color;
+
+
+
+  displaycolor = "red";
+
   return (
     <>
-    <div>
-      <div className="">
-      <div
-        className="animation-box"
-        style={{
-          backgroundColor: displayemotion,
-          borderRadius: "50%",
-          animationDuration: randomAnimationDulation + "s",
-          marginTop: 120,
-          height: randomsSize + 20,
-          width: randomsSize + 20,
-          position: "absolute",
-          right: randomWidth,
-          top: randomHeight,
-        }}
-      >
+      <div>
+        <div className="">
+          <div
+            className="animation-box"
+            style={{
+              backgroundColor: displaycolor,
+              borderRadius: "50%",
+              animationDuration: randomAnimationDulation + "s",
+              marginTop: 150,
+              height: randomsSize + 20,
+              width: randomsSize + 20,
+              position: "absolute",
+              right: randomWidth,
+              top: randomHeight,
+            }}
+          ></div>
+        </div>
       </div>
-      </div>
-    </div>
     </>
   );
 };
 
-export default RandomObject
+export default RandomObject;
